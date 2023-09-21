36a7dea4f7cc1f576c91ab3a3ca03443b3c9fdfe
xcelerate-design <xcelerate-design@outlook.com>
2023-09-20 21:32:20 -0400

    Added final Keyword

---
+++ src/main/java/com/xcelerateDesign/javaPrograms/finalKeyword/NumberPi.java
@@ -0,0 +1,18 @@
+package com.xcelerateDesign.javaPrograms.finalKeyword;
+
+/**
+ * @author Xcelerate Design on 2023-05-13
+ * @project java-programs
+ */
+
+public class NumberPi {
+
+    private final static double pi = 3.141592653589793;
+
+    public static void main(String[] args) {
+
+        System.out.println(NumberPi.pi);
+
+
+    }
+}
